32d794756f63e2704ff521f6172dd5df693d6124
Shashank Chandak <chandak.shashank16@gmail.com>
2019-07-14 23:48:25 +0530

    Updated Theme

--- app/src/main/java/com/shashank/passwordvalidationanimation/MainActivity.java
+++ app/src/main/java/com/shashank/passwordvalidationanimation/MainActivity.java
@@ -1,7 +1,9 @@
 package com.shashank.passwordvalidationanimation;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    void initialize(){
+
+
     }
 }
